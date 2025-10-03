08f028f5e79debc927ad58db2c6068dc290b2b93
= <=>
2025-10-03 11:22:35 +0530

    Put/Delete/Patch Mapping/ ResponseEntity learnings

--- Spring Learning/Spring-Learning/src/main/java/com/SpringLearn/Controller/EmployeeController.java
+++ Spring Learning/Spring-Learning/src/main/java/com/SpringLearn/Controller/EmployeeController.java
@@ -1,10 +1,16 @@
 package com.SpringLearn.Controller;
 
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,23 +30,69 @@ public class EmployeeController {
 	}
 
 	@GetMapping
-	public List<EmployeeDTO> getEmployees()
+	public ResponseEntity<List<EmployeeDTO>> getEmployees()
 	{
-		return serv.getEmployees();
+		return ResponseEntity.ok(serv.getEmployees());
 	}
 	
 	@GetMapping("{employeeId}")
-	public EmployeeDTO getEmployeeById(@PathVariable (name = "employeeId") int id )
+	public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable (name = "employeeId") int id )
 	{
 		
-		return serv.getEmployeeById(id);
+		EmployeeDTO savedEmp =  serv.getEmployeeById(id);
+		if(savedEmp == null) return ResponseEntity.notFound().build();
+		return ResponseEntity.ok(savedEmp);
 	}
 	
 
 	@PostMapping("/object")
-	public EmployeeDTO addEmployee(@RequestBody EmployeeDTO eml )
+	public ResponseEntity<EmployeeDTO> addEmployee(@RequestBody EmployeeDTO eml )
 	{
-		return serv.addEmployee(eml);
+		return new ResponseEntity<>(serv.addEmployee(eml),HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{employeeId}")
+	public EmployeeDTO updateEmployeeByID(@PathVariable int employeeId,@RequestBody EmployeeDTO empl)
+	{
+		return serv.updateEmployeeByID(employeeId,empl);
+	}
+	
+	@DeleteMapping("/{employeeId}")
+	public String deleteEmployeeById(@PathVariable int employeeId )
+	{
+		return serv.deleteEmployeeById(employeeId);
+	}
+	
+	@PatchMapping("/{employeeId}")
+	public EmployeeDTO partialUpdateEmployeeById(@PathVariable int employeeId, @RequestBody Map<String,Object> updates )
+	{
+		return serv.partialUpdateEmployeeById(employeeId,updates);
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Spring Learning/Spring-Learning/src/main/java/com/SpringLearn/Service/EmployeeService.java
+++ Spring Learning/Spring-Learning/src/main/java/com/SpringLearn/Service/EmployeeService.java
@@ -3,10 +3,14 @@ package com.SpringLearn.Service;
 import com.SpringLearn.DTO.EmployeeDTO;
 import com.SpringLearn.Entities.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.data.util.ReflectionUtils;
 import org.springframework.stereotype.Service;
 
 import com.SpringLearn.Repository.EmployeeRepository;
@@ -31,8 +35,8 @@ public class EmployeeService {
 	
 	public EmployeeDTO getEmployeeById(int id)
 	{
-		Employee employee = repo.findById(id).orElse(null);
-		return mp.map(employee, EmployeeDTO.class);
+		Optional<Employee> employee = repo.findById(id);
+		return employee.map(emp -> mp.map(emp, EmployeeDTO.class)).orElse(null);
 	}
 
 	public EmployeeDTO addEmployee(EmployeeDTO eml) {
@@ -42,4 +46,70 @@ public class EmployeeService {
 		return mp.map(savedEmpl, EmployeeDTO.class);
 	}
 
+	public EmployeeDTO updateEmployeeByID(int employeeId, EmployeeDTO empl) {
+
+		Employee empNew = mp.map(empl, Employee.class);
+		Employee getEmpl = repo.findById(employeeId).orElse(null);
+		if(getEmpl == null)
+		{
+			Employee emplSaved = repo.save(empNew);
+			return mp.map(emplSaved, EmployeeDTO.class);
+		}
+		empNew.setId(employeeId);
+		Employee emplSaved = repo.save(empNew);
+		return mp.map(emplSaved, EmployeeDTO.class);
+	}
+
+	public String  deleteEmployeeById(int employeeId) {
+		
+		boolean exists = repo.existsById(employeeId);
+		if(!exists)
+		{
+			return "Employee not found !";
+		}
+		
+		repo.deleteById(employeeId);
+		
+		return "Employee Deleted";
+		
+	}
+
+	public EmployeeDTO partialUpdateEmployeeById(int employeeId, Map<String, Object> updates) {
+
+		boolean exists = repo.existsById(employeeId);
+		if(!exists) return null;
+		
+		Employee savedEmpl = repo.findById(employeeId).get();
+		updates.forEach((key,value) -> {
+			
+			Field fieldToBeUpdated = ReflectionUtils.findField(Employee.class, f -> f.getName().equals(key));
+			fieldToBeUpdated.setAccessible(true);
+			ReflectionUtils.setField(fieldToBeUpdated, savedEmpl, value);
+			
+		});
+		return mp.map(repo.save(savedEmpl), EmployeeDTO.class);
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
